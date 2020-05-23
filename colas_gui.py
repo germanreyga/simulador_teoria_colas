ad374278ef43bc3930f20e9b90a0f8bd319c48c0
German <german.reyga@gmail.com>
2020-05-23 15:28:44 -0500

    MMsk & MG1 tabs and validations finished

--- colas_gui.py
+++ colas_gui.py
@@ -1,5 +1,10 @@
 import PySimpleGUI as sg
-from submit_logic import submit_mms, submit_mm1
+from submit_logic import submit_mms, submit_mm1, submit_mmsk, submit_mg1
+
+# General variables
+sigma_text = u"\u03C3"
+lambda_text = u"\u03BB"
+miu_text = u"\u03BC"
 
 # Defines the window theme
 sg.theme('Reddit')
@@ -9,8 +14,8 @@ LAMBDA_1 = "1_input_1"
 MIU_1 = "1_input_2"
 SUBMIT_1 = "Simular M/M/1"
 
-mm1_layout = [[sg.Text("Lambda (λ)"), sg.In(key=LAMBDA_1)],
-              [sg.Text("Miu (μ)"), sg.In(key=MIU_1)],
+mm1_layout = [[sg.Text(f"Lambda ({lambda_text})"), sg.In(key=LAMBDA_1)],
+              [sg.Text(f"Miu ({miu_text})"), sg.In(key=MIU_1)],
               [sg.Button(SUBMIT_1)]
               ]
 
@@ -20,10 +25,35 @@ MIU_2 = "2_input_2"
 SERVERS_2 = "2_input_3"
 SUBMIT_2 = "Simular M/M/s"
 
-mms_layout = [[sg.Text("Lambda (λ)"), sg.In(key=LAMBDA_2)],
-              [sg.Text("Miu (μ)"), sg.In(key=MIU_2)],
+mms_layout = [[sg.Text(f"Lambda ({lambda_text})"), sg.In(key=LAMBDA_2)],
+              [sg.Text(f"Miu ({miu_text})"), sg.In(key=MIU_2)],
               [sg.Text("Servidores (s)"), sg.In(key=SERVERS_2)],
               [sg.Button(SUBMIT_2)]
+              ]
+
+# Tab M/M/s
+LAMBDA_3 = "3_input_1"
+MIU_3 = "3_input_2"
+SERVERS_3 = "3_input_3"
+CAPACITY_3 = "3_input_4"
+SUBMIT_3 = "Simular M/M/s/k"
+
+mmsk_layout = [[sg.Text(f"Lambda ({lambda_text})"), sg.In(key=LAMBDA_3)],
+               [sg.Text(f"Miu ({miu_text})"), sg.In(key=MIU_3)],
+               [sg.Text("Servidores (s)"), sg.In(key=SERVERS_3)],
+               [sg.Text("Capacidad (k)"), sg.In(key=CAPACITY_3)],
+               [sg.Button(SUBMIT_3)]
+               ]
+
+# Tab M/G/1
+LAMBDA_4 = "4_input_1"
+MIU_4 = "4_input_2"
+SIGMA_4 = "4_input_4"
+SUBMIT_4 = "Simular M/G/1"
+mg1_layout = [[sg.Text(f"Lambda ({lambda_text})"), sg.In(key=LAMBDA_4)],
+              [sg.Text(f"Miu ({miu_text})"), sg.In(key=MIU_4)],
+              [sg.Text(f"Sigma ({sigma_text})"), sg.In(key=SIGMA_4)],
+              [sg.Button(SUBMIT_4)]
               ]
 
 
@@ -32,6 +62,8 @@ mms_layout = [[sg.Text("Lambda (λ)"), sg.In(key=LAMBDA_2)],
 general_layout = [[sg.TabGroup([[
     sg.Tab("M/M/1", mm1_layout),
     sg.Tab("M/M/s", mms_layout),
+    sg.Tab("M/M/s/k", mmsk_layout),
+    sg.Tab("M/G/1", mg1_layout)
 ]])]]
 
 window = sg.Window("Metodos cuantitativos y simulacion - Proyecto 2", general_layout)
@@ -46,4 +78,9 @@ if __name__ == '__main__':
             submit_mm1(sg=sg, la=values[LAMBDA_1], mi=values[MIU_1])
         if event == SUBMIT_2:
             submit_mms(sg=sg, la=values[LAMBDA_2], mi=values[MIU_2], s=values[SERVERS_2])
+        if event == SUBMIT_3:
+            submit_mmsk(sg=sg, la=values[LAMBDA_3], mi=values[MIU_3], s=values[SERVERS_3], k=values[CAPACITY_3])
+        if event == SUBMIT_4:
+            submit_mg1(sg=sg, la=values[LAMBDA_4], mi=values[MIU_4], sig=values[SIGMA_4])
+
     window.close()
--- submit_logic.py
+++ submit_logic.py
@@ -60,3 +60,73 @@ def submit_mm1(sg, la, mi):
     else:
         sg.Print("Error en los inputs:", text_color="red")
         sg.Print(incorrectInputs, text_color="red")
+
+
+def submit_mmsk(sg, la, mi, s, k):
+
+    inputsAreValid = True
+    incorrectInputs = ""
+
+    try:
+        la = float(la)
+        mi = float(mi)
+        s = int(s)
+        k = int(k)
+    except (ValueError):
+        sg.Print("Error de casteo. Lambda y Miu deben ser enteros o flotantes. Servidores y Capacidad debe ser entero.", text_color="red")
+        return
+
+    if la <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Lambda no puede ser negativo ni cero\n"
+    if mi <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Miu no puede ser negativo ni cero\n"
+    if s <= 1:
+        inputsAreValid = False
+        incorrectInputs += "* Servidores no puede ser menor o igual a uno\n"
+    if k <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Capacidad no puede ser negativo ni cero\n"
+
+    if inputsAreValid:
+        lae, l, lq, w, wq = mmsk(la, mi, s, k)
+        result = f"λe:{lae}\nL:{l}\nLq:{lq}\nW:{w}\nWq:{wq}"
+        sg.Print("Resultado de la simulacion M/M/s/k")
+        sg.Print(result, text_color="green")
+    else:
+        sg.Print("Error en los inputs:", text_color="red")
+        sg.Print(incorrectInputs, text_color="red")
+
+
+def submit_mg1(sg, la, mi, sig):
+
+    inputsAreValid = True
+    incorrectInputs = ""
+
+    try:
+        la = float(la)
+        mi = float(mi)
+        sig = float(sig)
+    except (ValueError):
+        sg.Print("Error de casteo. Lambda, Miu y Sigma deben ser enteros o flotantes", text_color="red")
+        return
+
+    if la <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Lambda no puede ser negativo ni cero\n"
+    if mi <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Miu no puede ser negativo ni cero\n"
+    if sig >= 1 or sig <= 0:
+        inputsAreValid = False
+        incorrectInputs += "* Sigma solo puede ser un valor entre 0 y 1 (no inclusivo)\n"
+
+    if inputsAreValid:
+        l, lq, w, wq = mg1(la, mi, sig)
+        result = f"L:{l}\nLq:{lq}\nW:{w}\nWq:{wq}"
+        sg.Print("Resultado de la simulacion M/G/1")
+        sg.Print(result, text_color="green")
+    else:
+        sg.Print("Error en los inputs:", text_color="red")
+        sg.Print(incorrectInputs, text_color="red")
